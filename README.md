9ad1a51e87e28d92a86e427c04c33da4e79e18f6
Maxwell Tucker <maxtuck21@gmail.com>
2015-07-26 23:19:12 -0700

    added chat and home commands to improve usability

---
+++ README.md
@@ -0,0 +1,16 @@
+07/24/15
+Maxwell Tucker
+
+Welcome to TermChat, a command line client for Facebook chat.
+
+Commands:
+
+- -c places you in a chat conversation with a specific user (Takes a user as an argument)
+- -h When in a chat with a user, takes you back to the default interface. (takes no arguments)
+
+TODO: 
+
+- error handling
+- support for group conversations
+- change prompt text
+- fix prompt behavior when messages are revieved 
--- index.js
+++ index.js
@@ -23,18 +23,46 @@ prompt.get(credentials, function(err, result) {
         if(err) return console.error(err);
         console.log('login success!');
         api.listen(function(err, message) {
+            console.log("\n");
             console.log(message.sender_name + ": " + message.body) 
         });
-        sendloop(api);
+        sendLoop(api);
     });
 });
 
-var sendloop = function(api) {
+var sendLoop = function(api) {
     prompt.get(toSend, function(err, result) {
-        api.getUserId(result.recipient, function(err, obj) {
-            if(err) console.error(err);
-            else api.sendMessage(result.message, obj[0].uid);
-            sendloop(api);
-        });
+        if(result.recipient.charAt(0) == "-" && result.recipient.charAt(1) == "c") {
+            sendUserLoop(api, result.recipient.slice(3), result.message);
+        }
+        else {
+            api.getUserId(result.recipient, function(err, obj) {
+                if(err) console.error(err);
+                else api.sendMessage(result.message, obj[0].uid);
+                sendLoop(api);
+            });
+        }
     });
 }
+
+var sendUserLoop = function(api, user, message) {
+    if(message != null) {
+        api.getUserId(user, function(err, obj) {
+            if(err) console.error(err);
+            else api.sendMessage(message, obj[0].uid);
+            sendUserLoop(api, user);
+        });
+    }
+    else {
+        var messagePrompt = "Message to " + user;
+        prompt.get({name: "message", description: messagePrompt}, function(err, result) {
+            if(result.message.charAt(0) == "-" && result.message.charAt(1) == "h") sendLoop(api);
+            else api.getUserId(user, function(err, obj) {
+                if(err) console.error(err);
+                else api.sendMessage(result.message, obj[0].uid);
+                sendUserLoop(api, user);
+            });
+        });
+    }
+}
+
